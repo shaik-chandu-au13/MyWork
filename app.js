de261427e9b5fcdb4f6b2b7a5e13da98c186544e
shaik chandu <shaikchandu24@gmail.com>
2021-03-02 21:10:23 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const port = process.env.PORT || 1234
+
+const app = express();
+app.get('/',(req, res) =>{
+    res.send("Health ok")
+})
+app.listen(port,(req,res) => {
+    console.log(`listening at ${port}`)
+});
